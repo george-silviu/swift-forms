d9a72332d52f16b1f035f50261b3cf589bdb75ce
unknown <costache.georgesilviu@yahoo.ro>
2024-11-28 17:03:34 +0200

    started building the login page

---
+++ client/src/pages/login.page.jsx
@@ -0,0 +1,14 @@
+import "./login.styles.scss";
+
+function LoginPage() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <p>Log in to continue your work</p>
+
+      <div></div>
+    </>
+  );
+}
+
+export default LoginPage;
