a0bac840a93c5ffa4788a5d2f43526598910f9b5
unknown <costache.georgesilviu@yahoo.ro>
2024-11-29 17:09:34 +0200

    finished v1 of register form

--- client/src/pages/login/login.page.jsx
+++ client/src/pages/login/login.page.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(email, password);
+    console.log(formFields);
     try {
       //perform request to login endpoint
     } catch (error) {}
@@ -30,36 +30,37 @@ const Login = () => {
     <section className="login-container">
       <form className="login-form">
         <header className="login-form-header">
-          <h1>Welcome</h1>
-          <p>Log in to continue your work</p>
+          <h1>Log in to SwiftForms</h1>
         </header>
 
-        <div className="form-input">
-          <label htmlFor="email">Email </label>
-          <input
-            id="email"
-            required
-            type="text"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
+        <div>
+          <div className="form-input">
+            <label htmlFor="email">Email </label>
+            <input
+              id="email"
+              required
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className="form-input">
-          <label htmlFor="password"> Password </label>
-          <input
-            id="password"
-            required
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
+          <div className="form-input">
+            <label htmlFor="password"> Password </label>
+            <input
+              id="password"
+              required
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
         <button className="button" onClick={handleSubmit}>
-          Submit
+          Login
         </button>
 
         <NavLink to="/register"> Register here</NavLink>
--- client/src/pages/register/register.page.jsx
+++ client/src/pages/register/register.page.jsx
@@ -1,5 +1,80 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import "./register.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+  repeatPassword: "",
+};
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password, repeatPassword } = formFields;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    console.log(formFields);
+  };
+
+  return (
+    <section className="register-page">
+      <form className="register-form">
+        <h1>Register to SwiftForms</h1>
+
+        <div>
+          <div className="form-input">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              value={password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="form-input">
+            <label htmlFor="repeatPassword">Repeat password</label>
+            <input
+              type="password"
+              id="repeatPassword"
+              name="repeatPassword"
+              required
+              value={repeatPassword}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <button className="button" onClick={handleRegister}>
+          Register
+        </button>
+
+        <NavLink to="/">Log in here</NavLink>
+      </form>
+    </section>
+  );
 };
 
 export default Register;
